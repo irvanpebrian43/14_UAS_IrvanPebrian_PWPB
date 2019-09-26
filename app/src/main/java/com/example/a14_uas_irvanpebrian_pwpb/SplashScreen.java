cb5133861d17abe47e0e59ec0d0564de1eb0fb9e
irvanpebrian43 <irvanpebrian43@gmail.com>
2019-09-26 13:46:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a14_uas_irvanpebrian_pwpb/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.a14_uas_irvanpebrian_pwpb;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                Intent pindah = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(pindah);
+                finish();
+            }
+        };
+
+        Handler handler = new Handler();
+        handler.postDelayed(run,1000);
+
+    }
+}
